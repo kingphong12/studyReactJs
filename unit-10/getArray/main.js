11bf092dc2ef5807307d12c2562b36d4cd2d44bc
kingphong12 <xuanphongkai12@gmail.com>
2020-12-12 22:16:31 +0700

    getArray

---
+++ unit-10/getArray/main.js
@@ -0,0 +1,22 @@
+const student = () => {
+    const studentNames = [
+        { id: 1, name: 'Nguyễn Thị Tèo' },
+        { id: 2, name: 'Phạm Văn Bưởi' },
+        { id: 3, name: 'Hoàng Văn Nam' },
+        { id: 4, name: 'Vũ Ngọc Duy' },
+        { id: 5, name: 'Nguyễn Minh Nhật' },
+        { id: 6, name: 'Phí Duy Quân' },
+        { id: 7, name: 'Trần Minh Minh' }
+    ]
+    const studentScores = [
+        { id: 1, score: 9.2 },
+        { id: 2, score: 2.3 },
+        { id: 3, score: 3.7 },
+        { id: 4, score: 6.9 },
+        { id: 5, score: 5.2 },
+        { id: 6, score: 9.6 },
+        { id: 7, score: 6.1 }
+    ]
+
+    console.log();
+}
