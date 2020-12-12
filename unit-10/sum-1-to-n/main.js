37b2aed68248e3ada53f8d9b5629b31d8aa7cec8
kingphong12 <xuanphongkai12@gmail.com>
2020-12-12 22:20:34 +0700

    sum 1 to n

--- unit-10/sum-1-to-n/main.js
+++ unit-10/sum-1-to-n/main.js
@@ -1,7 +1,7 @@
 function getSum(number) {
     if (typeof(number) !== 'number' || number <= 0 || number > 30) {
         return 'data khong hop le'
-    } 
+    }
     let sum = 0
     for (let i =1; i <= number; i++) {
         sum += i
