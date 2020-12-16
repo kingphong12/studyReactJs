29f9477763faeb2669b850a10a08a5011447f931
kingphong12 <xuanphongkai12@gmail.com>
2020-12-16 19:16:39 +0700

    update homework unit-11

--- unit-11/main.js
+++ unit-11/main.js
@@ -70,3 +70,33 @@ const data = {a: 1}
 const isObject = input => {
 	return !!input && input.constructor === Object
 }
+
+// Bài 9: Viết hàm nhập vào 1 array có cấu trúc như ví dụ sau:
+
+const students = [
+	{ id: 1, name: 'Nguyễn Thị Tèo', score: 9.2 },
+	{ id: 2, name: 'Phạm Văn Bưởi', score: 2.3 },
+	{ id: 3, name: 'Hoàng Văn Nam', score: 3.7 },
+	{ id: 4, name: 'Vũ Ngọc Duy', score: 6.9 },
+	{ id: 5, name: 'Nguyễn Minh Nhật', score: 5.2 },
+	{ id: 6, name: 'Phí Duy Quân', score: 9.6 },
+	{ id: 7, name: 'Trần Minh Minh', score: 6.1 }
+]
+
+// Hãy tạo 1 array mới, với cấu trúc sau:
+
+// ['Pass', 'Fail', 'Fail', 'Pass', 'Pass', 'Fail', 'Pass']
+// Điều kiện Pass:
+
+// Điểm trên 5.0 và không có chữ Duy là tên đệm
+
+const array =	students.map(item => { //hàm map() duyệt các phần tử trong mảng (item) trả về một mảng mới mà không làm thay đổi mảng cũ
+		if (item.score >= 5.0 && item.name.indexOf(' Duy ') === -1) {
+			return item = 'pass'
+		}
+		else {
+			return item = 'fail'
+		}
+	})
+
+	// bài 10: 
